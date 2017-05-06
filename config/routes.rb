44aaea6bce0731a953e236b2a929f99383347bf1
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-05-06 20:33:35 +1000

    upgrade layout of portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
-  get 'pages/contact'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
